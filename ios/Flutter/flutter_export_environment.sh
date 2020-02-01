423819dd107e74e9f6b1da194a8cdc01d8e72889
Christian Hass <christian.hass.muc@gmail.com>
2020-02-01 16:06:38 +0100

    Add generated files for ios.

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\thomas\AndroidStudioProjects\flutter_hackathon_aphasia"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/chris/dev/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/chris/DevProjects/flutter/FlutterHackathonAphasia"
+export "FLUTTER_TARGET=/Users/chris/DevProjects/flutter/FlutterHackathonAphasia/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\src\flutter\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/chris/dev/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
