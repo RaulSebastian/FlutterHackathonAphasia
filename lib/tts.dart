916e66852ec57d7a3a5e69866dd3df45627bbed8
Thomas Maier <mail@tommi-maier.de>
2020-02-01 15:47:16 +0100

    Again commit

---
+++ lib/tts.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+
+class TtsWidget extends StatelessWidget {
+  TtsWidget({Key key, this.listenableText}) : super(key: key);
+
+  final String listenableText;
+
+  @override
+  Widget build(BuildContext context) {
+    FlutterTts flutterTts = FlutterTts();
+    return Column(
+    mainAxisSize: MainAxisSize.min,
+    children: <Widget>[
+      IconButton(
+        icon: Icon(Icons.volume_up),
+        tooltip: 'Listen',
+        onPressed: () {
+          flutterTts.speak(listenableText);
+        },
+      )
+    ],
+  );
+  }
+
+}
