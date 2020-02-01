f72c48840cf1345bc624e3f454db88a9118f5c49
Raul Sebastian <raulschnelzer@gmail.com>
2020-02-01 16:59:50 +0100

    numbers

--- lib/numbers.dart
+++ lib/numbers.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_hackathon_aphasia/tts.dart';
-import 'package:intl/intl.dart';
 
 class NumbersPage extends StatefulWidget {
   NumbersPage({Key key, this.title}) : super(key: key);
@@ -18,39 +17,37 @@ class NumbersPageState extends State<NumbersPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.widgetTitle),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          children: <Widget>[
-            BasicDateField(),
-          ],
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.widgetTitle),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'Increment',
-        child: Icon(Icons.play_arrow),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Center(
+            // Center is a layout widget. It takes a single child and positions it
+            // in the middle of the parent.
+            child:
+                Numbers()) // This trailing comma makes auto-formatting nicer for build methods.
+        );
   }
 }
 
-class BasicDateField extends StatelessWidget {
-  final format = DateFormat("yyyy-MM-dd");
+class Numbers extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      FlatButton(
-          onPressed: () {},
-          child: Row(children: <Widget>[
-            Expanded(flex: 70, child: Text("1")),
-            Expanded(flex: 70, child: TtsWidget(listenableText: "1"))
-          ]))
-    ]);
+    final results = List.generate(10, (index) {
+      return Center(
+        child: Row(children: <Widget>[
+          Text(index.toString(), 
+            style: Theme.of(context).textTheme.display3),
+          TtsWidget(listenableText: index.toString(), language: "en-us",)
+        ]),
+      );
+    });
+
+    return GridView.count(
+        // Create a grid with 2 columns. If you change the scrollDirection to
+        // horizontal, this produces 2 rows.
+        crossAxisCount: 3,
+        children: results);
   }
 }
