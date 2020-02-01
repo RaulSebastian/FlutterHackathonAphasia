04e9e6aab0848ad511602d3809f1e26c68fb6c44
Raul Sebastian <raulschnelzer@gmail.com>
2020-02-01 16:17:52 +0100

    minor changes on tts

--- lib/tts.dart
+++ lib/tts.dart
@@ -3,23 +3,26 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 
 class TtsWidget extends StatelessWidget {
-  TtsWidget({Key key, this.listenableText}) : super(key: key);
+  TtsWidget({Key key, this.listenableText, this.language = 'de-DE'}) : super(key: key);
 
   final String listenableText;
+  final String language;
 
   @override
   Widget build(BuildContext context) {
-    FlutterTts flutterTts = FlutterTts();
     
     return
       IconButton(
         icon: Icon(Icons.volume_up),
         tooltip: 'Listen',
         onPressed: () async {
-          await flutterTts.setLanguage("en-US");
-          await flutterTts.setSpeechRate(1.0);
-          await flutterTts.setVolume(1.0);
-          await flutterTts.setPitch(1.0);
+          FlutterTts flutterTts = FlutterTts();
+          
+          flutterTts.setLanguage(language);
+          flutterTts.setSpeechRate(1.0);
+          flutterTts.setVolume(1.0);
+          flutterTts.setPitch(1.0);
+          await flutterTts.stop();
           await flutterTts.speak(listenableText);
         },
       );
