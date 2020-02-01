854d9e5a92c7b73d2e0e132798ad3403394409fe
Thomas Maier <mail@tommi-maier.de>
2020-02-01 12:35:03 +0100

    Added List for feature selection

--- lib/main.dart
+++ lib/main.dart
@@ -93,28 +93,26 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            ListTile(
+              leading: Icon(Icons.calendar_today),
+              title: Text("Calender"),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.access_time),
+              title: Text("Time"),
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.format_list_numbered),
+              title: Text("Numbers"),
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.title),
+              title: Text("Words"),
             ),
           ],
         ),
